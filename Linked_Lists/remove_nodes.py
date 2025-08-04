34868d816368ae5dddfc08d8c87cdce96ce87bbe
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-08-03 23:41:13 -0300

    Update remove_nodes

--- Linked_Lists/remove_nodes.py
+++ Linked_Lists/remove_nodes.py
@@ -4,6 +4,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+        
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         dummy = ListNode(0)
