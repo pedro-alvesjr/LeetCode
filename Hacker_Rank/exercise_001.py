de35de5559c3178b9c8849e01211b0203026e87e
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-08-30 23:12:06 -0300

    Create exercise_001.py

---
+++ Hacker_Rank/exercise_001.py
@@ -0,0 +1,26 @@
+import math
+import os
+import random
+import re
+import sys
+
+def aVeryBigSum(ar):
+    sum = 0
+    
+    for num in ar:
+        sum += num
+        
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
