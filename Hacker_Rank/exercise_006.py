ab13841bdd7b6eb78bafb43ee6bb8b1fb1a21ac3
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-09-05 23:42:26 -0300

    Create exercise_006.py

---
+++ Hacker_Rank/exercise_006.py
@@ -0,0 +1,35 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    tallestCandle = max(candles)
+    num_of_candles = 0
+    
+    for num in candles:
+        if num == tallestCandle:
+            num_of_candles += 1
+            
+    return num_of_candles
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
